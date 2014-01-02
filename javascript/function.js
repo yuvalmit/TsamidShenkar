d65c6449e0b0cd3638e03ffb5408738706cd9711
0xori <kadosh.ori@gmail.com>
2014-01-02 12:36:20 +0200

    change images path & added video.js

--- javascript/function.js
+++ javascript/function.js
@@ -5,23 +5,7 @@ function eyal(){
 
 $(document).ready(function(){
 
-	
-	var uploadBtn = $("#upload-btn");
-	uploadBtn.click(function(){
-		var input = $("#you-tube-input");
-		var embedCode = input.val();
-		var src="http://www.youtube.com/embed/EDVrTYW2l84";
-		var youtubeIframe = $(".youtube-player");
-		if(embedCode !== "undefined")
-		{
-		//	src = "http:"+embedCode;
-			youtubeIframe.src = src;
-		}
-		else
-		{
-			//TODO: show error
-		}
-	});
+
 
 });
 
