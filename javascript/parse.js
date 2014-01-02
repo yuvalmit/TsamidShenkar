16fefc1f302d264b60e3944fbf17d80dab49fa83
Idan Peleg <ipeleg@hotmail.com>
2014-01-02 11:59:16 +0200

    SignUp function added

--- javascript/parse.js
+++ javascript/parse.js
@@ -1,3 +1,21 @@
 Parse.initialize("hCiKNPSGy9q5iT40j0d9DAiLHpavkJMWxmsC15tS", "TmiPKzW632NWSIkuBB0Yj4HzYR4sJTba04k3iA8F");
 
-//IDAN
+function signUp (username, password, email) {
+	var user = new Parse.User();
+	
+	// Setting the new user credentials
+	user.set("username", username);
+	user.set("password", password);
+	user.set("email", email);
+
+	user.set("privileges", 2); // 1 Is for instructor 2 is for normal user
+
+	user.signUp(null, {
+  		success: function(user) {
+    		alert("Welcome " + username + " :)");
+  		},
+  		error: function(user, error) {
+    		alert("Error in signUp: " + error.code + " " + error.message);
+  		}
+	});
+}
