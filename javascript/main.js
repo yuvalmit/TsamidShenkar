b81d5baf6c602785c178787f278a0715c6e63087
EtaySchur <EtaySchur@gmail.com>
2014-01-10 11:41:18 +0200

    Etay commit
    update main page html & css
    make iFrame

---
+++ javascript/main.js
@@ -0,0 +1,20 @@
+
+
+$( document ).ready(function() {
+   $( "#my_zone_image" ).click(function() {
+
+
+   
+});
+
+      $( "#video_image" ).click(function() {
+ 
+});
+
+         $( "#game_image" ).click(function() {
+   
+});
+
+        
+  });
+ 
