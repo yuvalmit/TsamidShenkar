556dbb98614d96b9ee328fd4228fb795cd47d2e3
EtaySchur <EtaySchur@gmail.com>
2014-01-10 13:04:17 +0200

    ?

--- javascript/main.js
+++ javascript/main.js
@@ -1,22 +1,22 @@
 
 
 $( document ).ready(function() {
+
+
    $( "#my_zone_image" ).click(function() {
-
-
-   
+ 
 });
 
-      $( "#video_image" ).click(function() {
+    $( "#video_image" ).click(function() {
           $("#main_iframe").attr('src' , 'video-player.html');
          
 });
 
-         $( "#game_image" ).click(function() {
+    $( "#game_image" ).click(function() {
                 
 });
-          $( "#home" ).click(function() {
-                $("#main_iframe").attr('src' , 'start_page.html');
+    $( "#home" ).click(function() {
+          $("#main_iframe").attr('src' , 'start_page.html');
 });
 
         
