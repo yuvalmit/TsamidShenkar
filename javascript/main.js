b8d0fff28fd4e3a6c581571b8d28a9fe1af95760
EtaySchur <EtaySchur@gmail.com>
2014-01-10 13:02:17 +0200

    connect video page

--- javascript/main.js
+++ javascript/main.js
@@ -8,11 +8,15 @@ $( document ).ready(function() {
 });
 
       $( "#video_image" ).click(function() {
- 
+          $("#main_iframe").attr('src' , 'video-player.html');
+         
 });
 
          $( "#game_image" ).click(function() {
-   
+                
+});
+          $( "#home" ).click(function() {
+                $("#main_iframe").attr('src' , 'start_page.html');
 });
 
         
