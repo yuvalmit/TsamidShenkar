ec8752fbc2adb276bc29a9428523b10a1a37fa58
EtaySchur <EtaySchur@gmail.com>
2014-01-10 13:11:23 +0200

    fix

--- javascript/main.js
+++ javascript/main.js
@@ -1,7 +1,11 @@
 
 
 $( document ).ready(function() {
-
+  alert("document ready");
+   var user = new User;
+    var userName = user.getCurrentUser();
+    alert(userName);
+    alert(getCurrentUser().getName());
 
    $( "#my_zone_image" ).click(function() {
  
@@ -18,6 +22,8 @@ $( document ).ready(function() {
     $( "#home" ).click(function() {
           $("#main_iframe").attr('src' , 'start_page.html');
 });
+
+
 
         
   });
