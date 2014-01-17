d547b5996f9157aaf46eca4a3a590108640e14b1
EtaySchur <EtaySchur@gmail.com>
2014-01-17 13:38:56 +0200

    more fixes

--- javascript/start_page.js
+++ javascript/start_page.js
@@ -1,12 +1,18 @@
  function getCurrentLesson(data){
-        console.log(data);
+ 		if(data){
+ 				console.log(data);
+ 		}else{
+ 			
+ 			alert("No Misson for today !");
+ 		}
+        
     }
 
 
 $( document ).ready(function() {
 	$(".upload_button").click(function() {
         alert("Getting Today Mission");
-        getTodayLesson(getCurrentLesson());
+        getTodayLesson(getCurrentLesson);
     })
 
 
